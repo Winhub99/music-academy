f8fb7d31bf2d57fba8883d22692c044cf0981fbb
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-04-27 17:33:46 +0530

    nthn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import HeroSection from "@/components/ui/HeroSection";
-import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses"
-import courseData from "@/data/music_academy.json" 
 import WhyChooseUs from "@/components/WhyChooseUs";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +12,11 @@ export default function Home() {
       <HeroSection />
       <FeaturedCourses/>
       <WhyChooseUs/>
+      <MusicSchoolTestimonials/>
+      <UpcomingWebinars/>
+      <Instructors/>
+      <Footer/>
+
     </main>
   );
 }
